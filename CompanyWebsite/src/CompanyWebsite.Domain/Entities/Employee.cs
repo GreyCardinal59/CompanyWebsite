ec9604e0733b00ae330f1a5c00a2544296b637d5
GreyCardinal59 <grey59cardinal@gmail.com>
2025-07-04 19:40:38 +0500

    add domain models

---
+++ CompanyWebsite/src/CompanyWebsite.Domain/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanyWebsite.Domain.Entities;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    
+    public required string FullName { get; set; }
+
+    public required DateTime BirthDate { get; set; }
+
+    public required DateTime HireDate { get; set; }
+
+    public required decimal Salary { get; set; }
+
+    public required Guid DepartmentId { get; set; }
+    
+    public required Department Department { get; set; }
+}
